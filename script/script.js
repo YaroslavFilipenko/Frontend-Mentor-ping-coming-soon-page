4b8575382cffd892542be35f39e6b55794a33261
Yaroslav Filipenko <yarikfilipenko@gmail.com>
2023-06-09 17:30:25 +0300

    first commit
    Signed-off-by: Yaroslav Filipenko <yarikfilipenko@gmail.com>

---
+++ script/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    const formButton = $('.form__button');
+    const emailInput = $('.email');
+    const errorMessage = $('.error-message');
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+    formButton.on('click', function(e) {
+      e.preventDefault();
+  
+    if (emailInput == '' || !emailPattern.test(emailInput.val())) {
+        emailInput.addClass('error');
+        errorMessage.css('display', 'flex');
+    } else {
+        emailInput.removeClass('error');
+        errorMessage.css('display', 'none');
+        form.reset();
+    }
+    });
+});
